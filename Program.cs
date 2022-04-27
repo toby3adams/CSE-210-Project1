a646ee69e8dfddf036a68fc1a89a736c6fe05ffc
toby3adams <toby3adams@byui.edu>
2022-04-27 11:52:10 -0600

    designed tic tac toe grid

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// This my program thingy
 
+Console.WriteLine("yeet");
+
+char one = '1';
+char two = '2';
+char three = '3';
+char four = '4';
+char five = '5';
+char six = '6';
+char seven = '7';
+char eight = '8';
+char nine = '9';
+
+string grid = $"{one} | {two} | {three}\n----------\n{four} | {five} | {six}\n----------\n{seven} | {eight} | {nine}";
+
+
+
+Console.WriteLine(grid);
